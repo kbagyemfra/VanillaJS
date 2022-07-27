0961a42046ab8c721034ade072716d4e381b94c9
Kojo Agyemfra <kojo.agyemfra@latch.com>
2022-07-27 14:18:22 -0400

    real1-commit

---
+++ portfolio-web/index.js
@@ -0,0 +1,40 @@
+// const sections = document.querySelectorAll(".section");
+// const sectBtns = document.querySelectorAll(".controls");
+// const sectBtn = document.querySelectorAll(".control");
+// const allSections = document.querySelectorAll(".main-content");
+
+// const PageTransition = () => {
+//   // Btn click active class
+
+//   for (let i = 0; i < sectBtn.length; i++) {
+//     sectBtn[i].addEventListener("click", function () {
+//       let currentBtn = document.querySelectorAll(".active-btn");
+//       currentBtn[0].className = currentBtn[0].className.replace(
+//         "active-btn",
+//         ""
+//       );
+//       this.className += " active-btn";
+//     });
+//   }
+
+//   // Sections Active
+//   allSections.addEventListener("click", (e) => {
+//     console.log(e.target);
+//   });
+// };
+
+// PageTransition();
+
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
+    });
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+})();
